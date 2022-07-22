c9d467c067e12592ec84516e748b7c3c13d52b43
lluissabarich <sabarich@gmail.com>
2022-07-22 16:26:11 +0000

    canvis

--- quotes/index.php
+++ quotes/index.php
@@ -1,3 +1,3 @@
 <?php
-    print "This app returns a random funny quote.  HELLO_MESSAGE= $HELLO_MESSAGE    \n"
+    print "This app returns a random funny quote.  HELLO_MESSAGE= ${HELLO_MESSAGE}    \n"
 ?>
